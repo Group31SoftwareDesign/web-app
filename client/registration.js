430292636537f91a5d6b63a3fb9b32d97347d073
manmythmardo <alexc.mardoqueo@gmail.com>
2023-03-24 01:07:32 -0500

    organization
    lol

---
+++ client/registration.js
@@ -0,0 +1,6 @@
+// const message = "Hello, World!";
+// const firstDiv = document.getElementById("firstDiv");
+// const secondDiv = document.getElementById("secondDiv");
+
+// firstDiv.textContent = message.slice(0, Math.ceil(message.length / 2));
+// secondDiv.textContent = message.slice(Math.ceil(message.length / 2));
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express(); //holds the express code and wth which you will be interacting with the most. 
-const port = 3740;
+const port = 8000;
 //app.get takes 2 arguments path and any callback functions. 
+
 app.get('/', function (req, res) {
-    res.sendFile('login.html', {root: __dirname});
+    res.sendFile('FuelPurchaseHistory.html', {root: __dirname});
   });
+
   app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 }); 
