b5c14b6504c5aa6aa61c68971c19d53d056026a8
manmythmardo <alexc.mardoqueo@gmail.com>
2023-03-29 17:41:33 -0500

    fixed user profile

--- server.js
+++ server.js
@@ -33,7 +33,12 @@ app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index.ejs', { name: req.user.name })
+    // get the user's email address from the session
+    const email = req.user.email
+
+    // find the user with the matching email address
+    const user = users.find(user => user.email === email)
+  res.render('index.ejs', {user})
 });
 
 app.get('/public/css/styles.css', (req, res) => {
@@ -73,13 +78,50 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
-      password: hashedPassword
+      password: hashedPassword,
+      fullname: req.body.fullname,
+      address1: req.body.address1,
+      address2: req.body.address2,
+      city: req.body.city,
+      state: req.body.state,
+      zipcode: req.body.zipcode
     })
     res.redirect('/login')
   } catch {
     res.redirect('/register')
   }
 })
+
+app.post('/update-profile', checkAuthenticated, (req, res) => {
+  // get the user's email address from the session
+  const email = req.user.email
+
+  // get the updates from the form data
+  const updates = {
+    fullname: req.body.fullname,
+    address1: req.body.address1,
+    address2: req.body.address2,
+    city: req.body.city,
+    state: req.body.state,
+    zipcode: req.body.zipcode
+  }
+
+  // update the user's information
+  updateUser(email, updates)
+
+  // redirect to the user's profile page
+  res.redirect('/')
+})
+
+function updateUser(email, updates) {
+  // find the user with the matching email
+  const user = users.find(user => user.email === email)
+
+  // update the user's attributes with the new values
+  if (user) {
+    Object.assign(user, updates)
+  }
+}
 
 app.delete('/logout', (req, res) => {
   console.log('logout route called');
